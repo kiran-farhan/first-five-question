65364fdabf876bb3068711b0277495cc8688c4fe
kiran-farhan <159946236+kiran-farhan@users.noreply.github.com>
2024-03-14 00:20:10 +0500

    Add files via upload

---
+++ APP.TS.js
@@ -0,0 +1,9 @@
+// question num: 2
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var personname = "Hello Eric,";
+var message = " would you like to learn some Python today?”";
+console.log(personname + message);
+// question num:3
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var person_name = "KiranFarhan";
+console.log("lowercase:", person_name.toLowerCase);
